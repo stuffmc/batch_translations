5c3ae1cebd9f82116d2f22753a23e9df73025d6d
StuFF mc <mc@stuffmc.com>
2010-04-30 22:51:38 +0200

    Also removed globalize_ from the hidden field name and removed binding since the console was WARNING about it's obsolescence. BTW Not sure how "enable_nested_attributes" is used, not working, I called it from my model "manually.

--- lib/batch_translation.rb
+++ lib/batch_translation.rb
@@ -4,11 +4,11 @@ module ActionView
       def globalize_fields_for(locale, *args, &proc)
         raise ArgumentError, "Missing block" unless block_given?
         @index = @index ? @index + 1 : 1
-        object_name = "#{@object_name}[globalize_translations_attributes][#{@index}]"
+        object_name = "#{@object_name}[translations_attributes][#{@index}]"
         # Be sure to use @object.translation and not anymore .globalize_translations like in previous versions of this code since it's the way it works nowadays in Rails (writing at the time of 2.3.5).
         object = @object.translations.find_by_locale locale.to_s
-        @template.concat @template.hidden_field_tag("#{object_name}[id]", object ? object.id : ""), proc.binding
-        @template.concat @template.hidden_field_tag("#{object_name}[locale]", locale), proc.binding
+        @template.concat @template.hidden_field_tag("#{object_name}[id]", object ? object.id : "")#, proc.binding
+        @template.concat @template.hidden_field_tag("#{object_name}[locale]", locale)#, proc.binding
         @template.fields_for(object_name, object, *args, &proc)
       end
     end
@@ -21,7 +21,7 @@ module Globalize
       module Translated
         module Callbacks
           def enable_nested_attributes
-            accepts_nested_attributes_for :globalize_translations
+            accepts_nested_attributes_for :translations
           end
         end
         module InstanceMethods
